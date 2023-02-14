df3a8268fa4add9077d76409e8a43c4f34b9dc79
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-02-14 21:32:58 +0500

    adding changes when tasks are competed

--- todo_list/app/page.tsx
+++ todo_list/app/page.tsx
@@ -1,13 +1,50 @@
+'use client'
 import React, { useState } from 'react'
 
 export default function Page() {
 
+  const [todos, setTodos] = useState([
+    { thingsToDo: 'Start Learning Next Js', completed: true },
+    { thingsToDo: 'Complete', completed: false }
+  ]); /*thingsToDo and completed are two property of object stored in varibale todo */
+
+
+  const onClickHandler = (myCb) => {
+
+    const newThings = todos.map((todo) => {
+      console.log('Things to do: ', todo)
+      if (todo.thingsToDo == myCb.thingsToDo) {
+        todo.completed = !todo.completed; {/*Change the value to the opposite of what is stored*/ }
+      }
+      return todo;
+      console.log(newThings)
+      setTodos(newThings);
+
+    })
+  }
+  console.log(todos)
 
   return (
-    <div>
-      <h1 style={{ color: 'black', backgroundColor: 'whitesmoke' }}>Welcome to Todo List App</h1>
-      <input type="text" placeholder='Enter a task to add to the list' style={{ backgroundColor: 'peachpuff' }} />
-      <button style={{ borderColor: 'darkblue' }}> Add task</button>
-    </div>
+    <>
+      <div>
+
+        <h1 style={{ color: 'black', backgroundColor: 'whitesmoke' }}>Welcome to Todo List App</h1>
+        <input type="text" placeholder='Enter a task to add to the list' style={{ backgroundColor: 'peachpuff' }} />
+        <button style={{ borderColor: 'darkblue' }}> Add task</button>
+
+        <ul style={{ listStyle: 'none' }}>
+          {
+            todos.map((cb) => {
+              return <li style={{ color: cb.completed ? 'green' : 'red' }}
+                key={cb}>
+                <input type="checkbox" checked={cb.completed} onClick={() => { onClickHandler(cb) }} />
+                {cb.thingsToDo}
+
+              </li>; {/*will give error if thingsToDo is added */ }
+            })
+          }
+        </ul>
+      </div>
+    </>
   )
 }
