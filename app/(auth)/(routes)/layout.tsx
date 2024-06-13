4a6c8d0cf03073aeaee9f175d624674680e30c76
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-06-13 15:51:38 +0500

    installing shadcnui

--- app/(auth)/(routes)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div className='bg-gradient-to-bl from-[#ff7f50] via-[#f09c0b] to-[#ffc000] w-full h-screen'>
-            {children}
-        </div>
-    );
-}
-
-export default RootLayout;
