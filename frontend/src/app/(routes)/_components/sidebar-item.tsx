7be69a247d2021ddf6211c593cce09633a563a4b
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-06-15 17:44:43 +0500

    minor changes in the sidebar ui

--- frontend/src/app/(routes)/_components/sidebar-item.tsx
+++ frontend/src/app/(routes)/_components/sidebar-item.tsx
@@ -19,9 +19,9 @@ const SidebarItem = ({ title, href, icon: Icon }: Props) => {
 	return (
 		<button
 			className={cn(
-				"py-2.5 md:py-3 rounded-xl bg-gray-100/50 dark:bg-muted flex items-center justify-center gap-x-2  hover:scale-95  hover:transition hover:duration-300",
+				"py-2.5 md:py-3 rounded-xl bg-gray-100/50 dark:bg-muted flex items-center justify-center gap-x-2  hover:scale-95  hover:transition hover:duration-300 hover:border hover:border-green-500",
 				isActive &&
-					"bg-primary text-white hover:opacity-90 dark:bg-primary"
+					"bg-primary text-white hover:opacity-90 dark:bg-primary hover:border-none"
 			)}
 		>
 			<Icon size={24} />
