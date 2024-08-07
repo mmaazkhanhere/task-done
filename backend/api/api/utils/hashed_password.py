a28e0cbb952986b821a73fc65c3c33acb04d0e58
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-08-07 21:44:37 +0500

    dockerizing the app

--- backend/api/api/utils/hashed_password.py
+++
@@ -1,6 +0,0 @@
-import bcrypt
-
-def hashed_password(password:str)->str:
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password.decode('utf-8')
